5985becc20e78e5b9a86f56d522a2e47b6059ba8
Tam Nguyen <tamvnguyen.cc@gmail.com>
2018-04-23 15:06:51 -0700

    filtered out eth and bnb pairs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,18 +5,17 @@ const app = document.getElementById('app');
 
 const socket = new WebSocket('wss://stream.binance.com:9443/ws/!ticker@arr');
 
-socket.addEventListener('open', ((event) => {
-    console.log('ready');
-}))
-
 socket.addEventListener('message', ((event) => {
     let render = () => {
         let data = JSON.parse(event.data);
+        data = data.filter((currency)=>{
+            return currency.s.indexOf('BTC') !== -1;
+        }) 
         let template = data.map( (item)=>{
             return (
                 <div key={item.s}>
                     <h3 key={item.s + "name"}>{item.s}</h3>
-                    <p key={item.s + "price"}>{item.w}</p>
+                    <p key={item.s + "price"}>{item.c}</p>
                 </div>
             )
         })
