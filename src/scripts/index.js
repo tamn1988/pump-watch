acc89c7b300dc8c304a2505944867eab161b8991
Tam Nguyen <tamvnguyen.cc@gmail.com>
2018-04-24 22:44:34 -0700

    Somewhat functional, Need to restructure and organize code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,32 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../styles/styles.css';
+
 
 const app = document.getElementById('app');
-
 const socket = new WebSocket('wss://stream.binance.com:9443/ws/!ticker@arr');
 
+let startingData;
+
 socket.addEventListener('message', ((event) => {
-    let render = () => {
-        let data = JSON.parse(event.data);
-        data = data.filter((currency)=>{
-            return currency.s.indexOf('BTC') !== -1;
-        }) 
-        let template = data.map( (item)=>{
-            return (
-                <div key={item.s}>
-                    <h3 key={item.s + "name"}>{item.s}</h3>
-                    <p key={item.s + "price"}>{item.c}</p>
-                </div>
-            )
-        })
-        ReactDOM.render(template, app);
+
+    if (!startingData) {
+        initialData(event);
+        startingData = arrayToObject(startingData);
+
     }
-    render();
+    let filteredData = JSON.parse(event.data).filter((currency) => {
+        return currency.s.indexOf('BTC') !== -1;
+    })
+    filteredData.map((currency) => {
+        if (!startingData[currency.s]) {
+            startingData[currency.s] = currency;
+        }
+        startingData[currency.s]["change"] = round((((currency.c - startingData[currency.s]['c']) / startingData[currency.s]['c']) * 100), 2)
+    })
+
+    let template = Object.keys(startingData).map((key,i) => {
+        return (
+            <div className="container" key={startingData[key]['s']}>
+                <h3 key={startingData[key]['s'] + 'name'}>{startingData[key]['s']}</h3>
+                <p key={startingData[key]['s'] + 'change'}>{startingData[key]['change']}</p>
+            </div>
+        )
+        
+    })
+
+    ReactDOM.render(template, app);
+
+    let container = document.querySelectorAll('.container');
+    let paragraph = document.querySelectorAll('.container p')
+    
+for (let i = 0; i < paragraph.length; i++){
+    if (Number(paragraph[i].innerHTML) <  0){
+        container[i].classList.add('hide');
+    } else if (Number(paragraph[i].innerHTML) >  0){
+        container[i].classList.remove('hide');
+    }
+}
+
 }))
 
+const initialData = (data) => {
+    if (!startingData) {
+        startingData = JSON.parse(data.data).filter((currency) => {
+            return currency.s.indexOf('BTC') !== -1;
+        })
+    }
+}
+
+const arrayToObject = (array) =>
+    array.reduce((obj, item) => {
+        obj[item.s] = item
+        return obj
+    }, {})
 
 
-
+const round = (num, places)=>{
+    let multiplier = Math.pow(10, places);
+    return Math.round(num*multiplier) / multiplier;
+}
 
 
 
