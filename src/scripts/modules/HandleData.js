8f33a682b07a213a3914c9779414d873c61bf275
Tam Nguyen <tamn1988@gmail.com>
2018-06-11 11:40:34 -0700

    added data

--- src/scripts/modules/HandleData.js
+++ src/scripts/modules/HandleData.js
@@ -64,9 +64,81 @@ class HandleData {
 
 export default HandleData
 
-// const socket = new WebSocket('wss://stream.binance.com:9443/stream?streams=!ticker@arr/bnbbtc@trade/bnbbtc@depth20/bnbbtc@kline_15m');
-                             
-// socket.addEventListener('message', (({data})=>{
-//   let parsedData = JSON.parse(data);
-//   console.log(parsedData.stream)
-// }))
+// const buildApiLinks = (coin) => {
+//     return {
+//         tradeAPI: 'https://cors-anywhere.herokuapp.com/https://api.binance.com/api/v1/trades?symbol=' + coin + '&limit=35',
+//         bookAPI: 'https://cors-anywhere.herokuapp.com/https://api.binance.com/api/v1/depth?symbol=' + coin + '&limit=20',
+//         chartAPI: 'https://cors-anywhere.herokuapp.com/https://api.binance.com/api/v1/klines?symbol=' + coin + '&interval=15m&limit=500'
+//     }
+// }
+
+// const roundToTwo = (num, places) => {
+//     let multiplier = Math.pow(10, places);
+//     return Math.round(num * multiplier) / multiplier;
+// }
+
+// const convertDataTrade = (data) => {
+//     let tradeData = [];
+//     data.forEach((item) => {
+//         tradeData.push({
+//             price: roundToTwo(item.price, 9),
+//             qty: roundToTwo(item.qty, 6),
+//             time: new Date(item.time).toTimeString().slice(0, 8),
+//             id: item.id
+//         })
+//     })
+//     tradeData.reverse();
+//     return tradeData;
+// }
+
+// const convertDataBook = (data) => {
+//     let bookData = {
+//         bids: {},
+//         asks: {}
+//     }
+//     data.bids.forEach((item) => {
+//         bookData.bids.push({
+//             price: item[0],
+//             amount: roundToTwo(item[1], 6)
+//         })
+//     })
+//     data.asks.forEach((item) => {
+//         bookData.asks.push({
+//             price: item[0],
+//             amount: roundToTwo(item[1], 6)
+//         })
+//     })
+//     bookData.asks.reverse();
+//     return bookData;
+// }
+
+// const convertDataChart = (data) => {
+//     let chartData = data.map((item) => {
+//         return {
+//             close: Number(item[4]),
+//             time: item[6]
+//         }
+//     })
+//     return chartData;
+// }
+
+// const fetchData = (coinAPI, func) => {
+//     fetch(coinAPI)
+//         .then((response) => {
+//             if (!response.ok) {
+//                 throw Error(response.statusText);
+//             }
+//             return response.json();
+//         })
+//         .then((response) => {
+//            console.log(func(response));
+//         })
+//         .catch((error) => {
+//             console.log(error)
+//         })
+// }
+
+// const link = buildApiLinks('BNBBTC').tradeAPI;
+// const data = fetchData(link, convertDataTrade);
+// console.log(data);
+
