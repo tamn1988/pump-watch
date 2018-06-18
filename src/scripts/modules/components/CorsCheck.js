f76b57bb990803be41d491823e411b9a9bc953f9
Tam Nguyen <tamn1988@gmail.com>
2018-06-18 16:03:45 -0700

    Minor changes

--- src/scripts/modules/components/CorsCheck.js
+++ src/scripts/modules/components/CorsCheck.js
@@ -1,29 +1,29 @@
 import React from 'react';
 
-export default class CorsCheck extends React.Component{
-    constructor(props){
+export default class CorsCheck extends React.Component {
+    constructor(props) {
         super(props)
         this.handleChange = this.handleChange.bind(this);
-        this.state={
-            cors: false,
+        this.state = {
+            cors: 'no'
         }
     }
 
-    handleChange(e){
+    handleChange(e) {
         let value = e.target.value
-        this.setState({cors: value})
+        this.setState({ cors: value })
         this.props.handleChangeCors(value);
     }
 
-    render(){
+    render() {
         return (
-            <form className='flex flex--aligni-center flex--justify-end'>
+            <form className='flex flex--aligni-center flex--justify-end cors-form'>
                 <label>
-                    <input type="radio" checked={this.state.cors === 'no'} value={'no'} onChange={this.handleChange}/>
-                    No Cors Extension
+                    <input type="radio" checked={this.state.cors === 'no'} value={'no'} onChange={this.handleChange} />
+                    No Extension
                 </label>
                 <label>
-                    <input type="radio" checked={this.state.cors === 'yes'} value={'yes'} onChange={this.handleChange}/>
+                    <input type="radio" checked={this.state.cors === 'yes'} value={'yes'} onChange={this.handleChange} />
                     Cors Extension
                 </label>
             </form>
